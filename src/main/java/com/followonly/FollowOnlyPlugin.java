be9f8e5a2e9550ab5740b1001dc8817be99c58f3
Jarrod Ferriss <w0856197@myscc.ca>
2024-11-11 14:52:45 -0500

    Initial config

--- src/main/java/com/followonly/FollowOnlyPlugin.java
+++ src/main/java/com/followonly/FollowOnlyPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.followonly;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Follow Only"
 )
-public class ExamplePlugin extends Plugin
+public class FollowOnlyPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private FollowOnlyConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	FollowOnlyConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(FollowOnlyConfig.class);
 	}
 }
